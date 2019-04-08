3952f41e9d013d5ef508698f36178b7ea4897780
SiL3x <raven2020@gmx.de>
2019-04-08 21:17:24 +0200

    added substrate test790

--- src/main/java/com/mk/configuration/SubstrateFactory.java
+++ src/main/java/com/mk/configuration/SubstrateFactory.java
@@ -3,6 +3,7 @@ package com.mk.configuration;
 import com.mk.configuration.substrates.Plane790;
 import com.mk.configuration.substrates.Realistic1000;
 import com.mk.configuration.substrates.SubstrateInterface;
+import com.mk.configuration.substrates.Test5_790;
 
 public class SubstrateFactory {
     public static final String PLANE790 = "Plane790";
@@ -15,6 +16,8 @@ public class SubstrateFactory {
                 return new Plane790();
             case REALISTIC1000:
                 return new Realistic1000();
+            case TEST5_790:
+                return new Test5_790();
             default:
                 throw new IllegalStateException("Don't know about this substrate yet!");
         }
